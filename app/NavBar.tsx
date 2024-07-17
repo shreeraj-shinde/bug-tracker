3830a1d39b2e2dc328c81a4a84b2eaacd9ffa15d
shreeraj-shinde <shreerajshinde86@gmail.com>
2024-07-17 13:15:06 +0530

    model created

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,22 +1,30 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 import { FaBug } from "react-icons/fa";
+import classNames from "classnames";
+import { usePathname } from "next/navigation";
 
 const NavBar = () => {
+  const currentPath = usePathname();
   const links = [
     { label: "Dashboard", href: "/" },
-    { label: "Bugs", href: "/issues" },
+    { label: "Bugs", href: "/bugs" },
   ];
 
   return (
-    <nav className="flex space-x-6 border-b-2 px-6 h-14 items-center mb-5 bg-black">
+    <nav className="flex space-x-6 border-b-2 px-6 h-14 items-center mb-5 bg-zinc-800">
       <Link className="text-white" href={"/"}>
         <FaBug />
       </Link>
       <ul className="flex space-x-6">
         {links.map((link, index) => (
           <Link
-            className="text-zinc-300 hover:text-white transition-colors font-semibold"
+            className={classNames({
+              "text-white": currentPath === link.href,
+              "text-zinc-300": currentPath != link.href,
+              "hover:text-white font-medium transition-colors": true,
+            })}
             key={index}
             href={link.href}
           >
